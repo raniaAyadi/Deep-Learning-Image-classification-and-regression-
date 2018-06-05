5cb87824120b6909acc0ac253b6c64d06a121f47
raniaAyadi <rania.ayadi.ar@gmail.com>
2018-06-05 11:38:19 +0200

    EXO1

---
+++ mp2.py
@@ -0,0 +1,121 @@
+import matplotlib.pyplot as plt
+# matplotlib inline
+import numpy as np
+import sys
+
+def generate_a_drawing(figsize, U, V, noise=0.0):
+    fig = plt.figure(figsize=(figsize,figsize))
+    ax = plt.subplot(111)
+    plt.axis('Off')
+    ax.set_xlim(0,figsize)
+    ax.set_ylim(0,figsize)
+    ax.fill(U, V, "k")
+    fig.canvas.draw()
+    imdata = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)[::3].astype(np.float32)
+    imdata = imdata + noise * np.random.random(imdata.size)
+    plt.close(fig)
+    return imdata
+
+def generate_a_rectangle(noise=0.0, free_location=False):
+    figsize = 1.0    
+    U = np.zeros(4)
+    V = np.zeros(4)
+    if free_location:
+        corners = np.random.random(4)
+        top = max(corners[0], corners[1])
+        bottom = min(corners[0], corners[1])
+        left = min(corners[2], corners[3])
+        right = max(corners[2], corners[3])
+    else:
+        side = (0.3 + 0.7 * np.random.random()) * figsize
+        top = figsize/2 + side/2
+        bottom = figsize/2 - side/2
+        left = bottom
+        right = top
+    U[0] = U[1] = top
+    U[2] = U[3] = bottom
+    V[0] = V[3] = left
+    V[1] = V[2] = right
+    imdata = generate_a_drawing(figsize, U, V, noise)
+    imdatann = generate_a_drawing(figsize, U, V)	
+    return [imdata, imdatann]
+
+	
+def generate_a_rectangle1(noise=0.0, free_location=False):
+    figsize = 1.0    
+    U = np.zeros(4)
+    V = np.zeros(4)
+    if free_location:
+        corners = np.random.random(4)
+        top = max(corners[0], corners[1])
+        bottom = min(corners[0], corners[1])
+        left = min(corners[2], corners[3])
+        right = max(corners[2], corners[3])
+    else:
+        side = (0.3 + 0.7 * np.random.random()) * figsize
+        top = figsize/2 + side/2
+        bottom = figsize/2 - side/2
+        left = bottom
+        right = top
+    U[0] = U[1] = top
+    U[2] = U[3] = bottom
+    V[0] = V[3] = left
+    V[1] = V[2] = right
+    return generate_a_drawing(figsize, U, V, noise)
+	
+
+def generate_a_disk(noise=0.0, free_location=False):
+    figsize = 1.0
+    if free_location:
+        center = np.random.random(2)
+    else:
+        center = (figsize/2, figsize/2)
+    radius = (0.3 + 0.7 * np.random.random()) * figsize/2
+    N = 50
+    U = np.zeros(N)
+    V = np.zeros(N)
+    i = 0
+    for t in np.linspace(0, 2*np.pi, N):
+        U[i] = center[0] + np.cos(t) * radius
+        V[i] = center[1] + np.sin(t) * radius
+        i = i + 1
+    imdata = generate_a_drawing(figsize, U, V, noise)
+    imdatann = generate_a_drawing(figsize, U, V)	
+    return [imdata, imdatann]
+
+def generate_a_triangle(noise=0.0, free_location=False):
+    figsize = 1.0
+    if free_location:
+        U = np.random.random(3)
+        V = np.random.random(3)
+    else:
+        size = (0.3 + 0.7 * np.random.random())*figsize/2
+        middle = figsize/2
+        U = (middle, middle+size, middle-size)
+        V = (middle+size, middle-size, middle-size)
+    imdata = generate_a_drawing(figsize, U, V, noise)
+    imdatann = generate_a_drawing(figsize, U, V)	
+    return [imdata, imdatann]
+
+	
+def generate_dataset_classification(nb_samples, noise=0.0, free_location=False):
+    # Getting im_size:
+    im_size = generate_a_rectangle1().shape[0]
+    X = np.zeros([nb_samples,im_size])
+    Xnn = np.zeros([nb_samples,im_size])
+    Y = np.zeros(nb_samples)
+    print('Creating data:')
+    for i in range(nb_samples):
+        if i % 10 == 0:
+            print(i)
+        category = np.random.randint(3)
+        if category == 0:
+            [X[i], Xnn[i]] = generate_a_rectangle(noise, free_location)
+        elif category == 1: 
+            [X[i], Xnn[i]] = generate_a_disk(noise, free_location)
+        else:
+            [X[i], Xnn[i]] = generate_a_triangle(noise, free_location)
+        Y[i] = category
+    X = (X + noise) / (255 + 2 * noise)
+    Xnn = (Xnn) / (255)
+    return [X, Xnn, Y]
